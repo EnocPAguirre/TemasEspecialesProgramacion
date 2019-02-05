17b8ac16652651d28c679fc8a3ff177cad6630f0
Enoc <enocpinedaa@gmail.com>
2019-02-05 15:21:03 -0600

    Clases Dos Temas Especiales de Programación y Computación

---
+++ ejemplo1.php
@@ -0,0 +1,26 @@
+<html>
+      <body>
+<?php
+
+//puede ser de la siguiente manera <? ?/>
+
+  //Definir variables, hace un salto de linea
+  $variable = 'hola \n';
+
+  //método echo, la diferencia es que se procesa
+  echo $variable;
+
+  $otra = 'mundo';
+  echo $otra
+
+  //Ciclo for
+  for ($i=; $i  <5 ; $i+) {
+    //Imprimir etiquetas de html, se acostumbra a usar punto
+    echo "<h".$i.">".$i."</h".$i.">";
+
+  }
+
+//Generar código html
+?>
+      </body>
+</html>
